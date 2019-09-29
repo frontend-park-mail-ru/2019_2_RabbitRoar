9f70c70cc02c89a898e511d9a28bf7ba505cfbf3
Egor <egor140398@gmail.com>
2019-09-29 22:54:22 +0300

    make autorisation request

--- frontend/src/js/autorisation/autorisation_click.js
+++ frontend/src/js/autorisation/autorisation_click.js
@@ -1,19 +1,34 @@
 import {createMainMenu} from '../main_menu/main_menu.js';
+import {ajax} from '../requests/ajax.js';
 
 export function setAutorisationListeners() {
-    document.getElementById('autorisation').addEventListener('click', function (event) {
-        const login = document.getElementById('autorisation').textContent;
-        if (login == '') {
-            return
-        }
+    const element = document.getElementById('autorisation');
+    element.addEventListener('click', function (event) {
+    
+        const login = document.getElementById('login').value;
+        const password = document.getElementById('password').value;
 
-        const password = document.getElementById('password').textContent;
-        if (password == '') {
-            return
-        }
+        event.preventDefault();
 
-        document.cookie = "autorised=true";
-        createMainMenu();
-        // //Отправляем на сервер логин с паролем, получаем НОВОГО пользователя
+
+        alert("begin");
+        ajax(
+            'POST',
+            'http://localhost:3000/user/login',
+            {login, password},
+            function (status, response) {
+                alert("Пришло ебать");
+                if (status === 200) {
+                    document.cookie = "autorised=true";
+                    createMainMenu();
+                    alert("успех");
+                } else {
+                    const {error} = JSON.parse(response);
+                    alert(error);
+                }
+            }
+        );
+        alert("end");
+
     }); 
 }
