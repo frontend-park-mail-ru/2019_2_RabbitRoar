380b1d6433cee5dab2570617af2754d75cc24310
Anita <smirnovasiberia@gmail.com>
2019-09-30 16:25:05 +0300

    Editing done.

--- frontend/src/js/profile/profile_click.js
+++ frontend/src/js/profile/profile_click.js
@@ -1,10 +1,26 @@
+import {ajax} from '../requests/ajax.js';
+
 export function setProfileListeners() {
     document.getElementById('save-button').addEventListener('click', function (event) {
         for (let valId of changedForms.keys()) {
             console.log(valId, changedForms.get(valId));
             console.log(changedForms.get(valId));
         }
-        // формируем json с измененными данными и отправлем его на сервер
+        event.preventDefault();
+
+        ajax(
+            'PUT',
+            'http://localhost:3000/user',
+            JSON.stringify(mapToObj(changedForms)),
+            function (status, response) {
+                if (status === 200) {
+                    createMainMenu();
+                } else {
+                    const {error} = JSON.parse(response);
+                    alert(error);
+                }
+            }
+        );
         changedForms.clear()
         console.log("Сохранение");
     });
@@ -27,3 +43,13 @@ let changedForms = new Map();
 const changedValue = (changedVal, valId) => {
     changedForms.set(valId, changedVal);
 }
+
+function mapToObj(inputMap) {
+    let obj = {};
+
+    inputMap.forEach(function(value, key){
+        obj[key] = value
+    });
+
+    return obj;
+} 
