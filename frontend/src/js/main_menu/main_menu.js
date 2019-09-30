b785a734a8e01858fed345af8fe665d735c3ba7f
Anita <smirnovasiberia@gmail.com>
2019-09-30 17:25:42 +0300

    Cookie bug fixed.

--- frontend/src/js/main_menu/main_menu.js
+++ frontend/src/js/main_menu/main_menu.js
@@ -1,16 +1,16 @@
-import {setTabListeners} from './events/tab_click.js';
-import {setNavbarListeners} from './events/navbar_click.js';
+import { setTabListeners } from './events/tab_click.js';
+import { setNavbarListeners } from './events/navbar_click.js';
 
-import {RoomCreator} from './creators/room_creator.js'
-import {BestCreator} from './creators/best_creator.js'
-import {NavbarCreator} from './creators/navbar_creator.js'
+import { RoomCreator } from './creators/room_creator.js'
+import { BestCreator } from './creators/best_creator.js'
+import { NavbarCreator } from './creators/navbar_creator.js'
 
-import {getCookie} from "../cookie/cookie.js";
+import { getCookie } from "../cookie/cookie.js";
 
 
 import Tabs from '../../templates/main_menu/tabs.pug';
 
-import {createRegistration} from '../registration/registration_creator.js';
+import { createRegistration } from '../registration/registration_creator.js';
 
 
 //ID у табов являются ключами мапы!
@@ -18,7 +18,8 @@ export const contentCreators = new Map([['rooms', RoomCreator], ['top', BestCrea
 
 
 export function createMainMenu(creatorType = 'top') {
-    if (getCookie("id") == "") {
+    const cookie = getCookie("id");
+    if ((cookie == "") || (cookie == undefined)) {
         createRegistration();
         return;
     }
