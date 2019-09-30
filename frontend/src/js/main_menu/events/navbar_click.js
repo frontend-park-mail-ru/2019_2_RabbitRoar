cbb85d71da2aacf9803c49144befbbaf02a08bbc
Alexey Navarkin <navarka.all@gmail.com>
2019-09-30 19:04:43 +0300

    Fix braces.

--- frontend/src/js/main_menu/events/navbar_click.js
+++ frontend/src/js/main_menu/events/navbar_click.js
@@ -1,31 +1,33 @@
-import {createProfile} from '../../profile/profile_creator.js';
-import {createRegistration} from '../../registration/registration_creator.js';
-import {createAutorisation} from '../../autorisation/autorisation_creator.js';
-import {createMainMenu} from '../main_menu.js';
-import {getCookie, setCookie} from "../../cookie/cookie.js";
+import { createProfile } from '../../profile/profile_creator.js';
+import { createRegistration } from '../../registration/registration_creator.js';
+import { createAutorisation } from '../../autorisation/autorisation_creator.js';
+import { createMainMenu } from '../main_menu.js';
+import { getCookie, setCookie } from "../../cookie/cookie.js";
 
-import {ajax} from '../../requests/ajax.js';
+import { ajax } from '../../requests/ajax.js';
 
 export function setNavbarListeners() {
-    let element = document.querySelector('.navbar__game-logo')
+    let element = document.querySelector('.navbar__game-logo');
     if (element) {
         element.addEventListener('click', function (event) {
-        event.preventDefault();
-        createMainMenu();
-    });
+            event.preventDefault();
+            createMainMenu();
+        });
+    }
 
     element = document.querySelector('.navbar__user-logo')
     if (element) {
         element.addEventListener('click', function (event) {
-        event.preventDefault();
-        if (getCookie("id") == "") {
-            createRegistration();
-        } else {
-            createProfile();
-        }
-    });
+            event.preventDefault();
+            if (getCookie("id") == "") {
+                createRegistration();
+            } else {
+                createProfile();
+            }
+        });
+    };
 
-    let element = document.getElementById('nav_exit');
+    element = document.getElementById('nav_exit');
     if (element) {
         element.addEventListener('click', function (event) {
             event.preventDefault();
@@ -34,12 +36,12 @@ export function setNavbarListeners() {
             ajax(
                 'GET',
                 'http://frontend.photocouple.space:3000/user/logout',
-                {id},
+                { id },
                 function (status, response) {
                     if (status === 200) {
                         createRegistration();
                     } else {
-                        const {error} = JSON.parse(response);
+                        const { error } = JSON.parse(response);
                         console.log(error);
                     }
                 }
