7a9d45794d18258f5887f18043d3f8c13f0c5436
Anita <smirnovasiberia@gmail.com>
2019-10-21 00:58:20 +0300

    Profile validation is working, logic was moved to form_validation.js.

--- frontend/src/js/controller/profileC.js
+++ frontend/src/js/controller/profileC.js
@@ -2,7 +2,8 @@ import UserValidatorF from "../fasade/userValidatorF.js";
 import { DomEventsWrapperMixin } from "../DomEventsWrapperMixin.js";
 import Bus from "../event_bus.js";
 import { PROFILE_UPDATE } from "../modules/events.js";
-import { emailIsValid, usernameIsValid, passwordsAreEqual, passwordIsValid } from "../modules/form_validation.js";
+import { fileVaildation, textFieldsVaildation } from "../modules/form_validation.js";
+
 
 class ProfileC {
     constructor() {
@@ -31,71 +32,12 @@ class ProfileC {
     }
 
     _saveTextFields() {
-        if (this.changedForms.size == 0) {
-            const errorMain = document.getElementById("error_main");
-            this._deleteErrorCssClasses(errorMain);
-            errorMain.classList.add("error-visible");
-            errorMain.innerHTML = "Нет изменений для сохранения.";
-            return;
+        const result = textFieldsVaildation(this.changedForms);
+        if (result.fieldsAreValid) {
+            UserValidatorF.changeTextFields(result.changes);
+            this.changedForms.clear();
+            Bus.emit(PROFILE_UPDATE);
         }
-        const changes = {};
-
-        const passwordWasEntered = this.changedForms.has("password");
-        const passwordConfirmationWasEntered = this.changedForms.has("password-confirmation");
-
-        if (passwordWasEntered && passwordConfirmationWasEntered) {
-            const errorPasswordElement = document.getElementById("error_password");
-            if (passwordsAreEqual(this.changedForms.get("password"), this.changedForms.get("password-confirmation"))) {
-                if (passwordIsValid(password)) {
-                    changes["password"] = this.changedForms.get("password");
-                } else {
-                    this._deleteErrorCssClasses(errorPasswordElement);
-                    errorPasswordElement.classList.add("error-visible");
-                    errorPasswordElement.innerHTML = "Недопустимый пароль. Введите минимум 5 символов.";
-                    return;
-                }
-            } else {
-                this._deleteErrorCssClasses(errorPasswordElement);
-                errorPasswordElement.classList.add("error-visible");
-                errorPasswordElement.innerHTML = "Пароли не совпадают.";
-                return;
-            }
-        } else if (passwordWasEntered || passwordConfirmationWasEntered) {
-            this._deleteErrorCssClasses(errorPasswordElement);
-            errorPasswordElement.classList.add("error-visible");
-            errorPasswordElement.innerHTML = "Для смены пароля заполните два поля.";
-            return;
-        }
-        this.changedForms.delete("password");
-        this.changedForms.delete("password-confirmation");
-
-
-        if (this.changedForms.has("username")) {
-            const username = this.changedForms.get("username");
-            if (!usernameIsValid(username)) {
-                const errorUsernameElement = document.getElementById("error_username");
-                this._deleteErrorCssClasses(errorUsernameElement);
-                errorUsernameElement.classList.add("error-visible");
-                errorUsernameElement.innerHTML = "Недопустимый username.";
-                document.getElementById("username").classList.add("input-error");
-                return;
-            }
-        }
-
-        if (this.changedForms.has("email")) {
-            const email = this.changedForms.get("email");
-            if (!emailIsValid(email)) {
-                const errorEmailElement = document.getElementById("error_email");
-                this._deleteErrorCssClasses(errorEmailElement);
-                errorEmailElement.classList.add("error-visible");
-                errorEmailElement.innerHTML = "Недопустимый email.";
-                return;
-            }
-        }
-
-        UserValidatorF.changeTextFields(changes);
-        this.changedForms.clear();
-        Bus.emit(PROFILE_UPDATE);
     }
 
     _cansel() {
@@ -126,68 +68,12 @@ class ProfileC {
 
     _saveImage() {
         let formData = new FormData();
-        const input = document.querySelector(".profile__download-img");
-        if (this._checkFileSize(input.files[0]) === false) {
-            return;
-        }
-        const errorFileElement = document.getElementById("error_top");
-        if (this._checkFileType(input.files[0]) === false) {
-            this._deleteErrorCssClasses(errorFileElement);
-            errorFileElement.classList.add("error-visible");
-            errorFileElement.innerHTML = "Файл недопустимого расширения и загружен не будет.";
-        } else {
-            this._deleteErrorCssClasses(errorFileElement);
-            errorFileElement.classList.add("file-downloaded");
-            errorFileElement.innerHTML = "Файл загружен";
+        const fileIsValid = fileVaildation();
+
+        if (fileIsValid) {
             formData.append("userfile", input.files[0]);
             UserValidatorF.changeUserAvatar(formData);
         }
-    }
-
-    _checkFileSize(file) {
-        if (file.size > 2000000) {
-            const errorFileElement = document.getElementById("error_top");
-            this._deleteErrorCssClasses(errorFileElement);
-            errorFileElement.classList.add("error-visible");
-            errorFileElement.innerHTML = "Размер файла не должен привышать 2МБ";
-            return false;
-        }
-        return true;
-    }
-
-    _checkFileType(blob) {
-        const fileReader = new FileReader();
-        let type;
-        fileReader.onloadend = (e) => {
-            const arr = (new Uint8Array(e.target.result)).subarray(0, 4);
-            let header = "";
-            for (let i = 0; i < arr.length; i++) {
-                header += arr[i].toString(16);
-            }
-            switch (header) {
-                // png
-                case "89504e47":
-                    type = "png";
-                    break;
-
-                // jpeg
-                case "ffd8ffe0":
-                case "ffd8ffe1":
-                case "ffd8ffe2":
-                case "ffd8ffe3":
-                case "ffd8ffe8":
-                    type = "jpeg";
-                    break;
-                default:
-                    type = "wrong_type";
-                    break;
-            }
-        };
-        fileReader.readAsArrayBuffer(blob);
-        if (type == "wrong_type") {
-            return false;
-        }
-        return true;
     }
 }
 
--- frontend/src/js/modules/form_validation.js
+++ frontend/src/js/modules/form_validation.js
@@ -63,7 +63,7 @@ const _fieldValidation = (value, valueInputElement, errorElement, validFunction,
     return fieldIsOk;
 };
 
-export function registrationValidation(errorPasswordElement, errorUsernameElement, errorEmailElement) {
+export const registrationValidation = (errorPasswordElement, errorUsernameElement, errorEmailElement) => {
     let registrationError = _fieldValidation(document.getElementById("username").value, document.getElementById("username"),
         errorUsernameElement, _usernameIsValid, "Введите логин.", "Логин должен содержать минимум 3 символа.");
 
@@ -77,4 +77,165 @@ export function registrationValidation(errorPasswordElement, errorUsernameElemen
         return false;
     }
     return true;
-}
+};
+
+export const fileVaildation = () => {
+    const input = document.querySelector(".profile__download-img");
+    if (!_checkFileSize(input.files[0])) {
+        return false;
+    }
+    const errorFileElement = document.getElementById("error_top");
+    if (!_checkFileType(input.files[0])) {
+        _deleteCssClassesFileInput(errorFileElement);
+        errorFileElement.classList.add("error-visible");
+        errorFileElement.innerHTML = "Файл недопустимого расширения и загружен не будет.";
+        return false;
+    }
+
+    _deleteCssClassesFileInput(errorFileElement);
+    errorFileElement.classList.add("file-downloaded");
+    errorFileElement.innerHTML = "Файл загружен";
+    return true;
+};
+
+const _checkFileSize = (file) => {
+    if (file.size > 2000000) {
+        const errorFileElement = document.getElementById("error_top");
+        _deleteCssClassesFileInput(errorFileElement);
+        errorFileElement.classList.add("error-visible");
+        errorFileElement.innerHTML = "Размер файла не должен привышать 2МБ";
+        return false;
+    }
+    return true;
+};
+
+const _checkFileType = (blob) => {
+    const fileReader = new FileReader();
+    let type;
+    fileReader.onloadend = (e) => {
+        const arr = (new Uint8Array(e.target.result)).subarray(0, 4);
+        let header = "";
+        for (let i = 0; i < arr.length; i++) {
+            header += arr[i].toString(16);
+        }
+        switch (header) {
+            // png
+            case "89504e47":
+                type = "png";
+                break;
+
+            // jpeg
+            case "ffd8ffe0":
+            case "ffd8ffe1":
+            case "ffd8ffe2":
+            case "ffd8ffe3":
+            case "ffd8ffe8":
+                type = "jpeg";
+                break;
+            default:
+                type = "wrong_type";
+                break;
+        }
+    };
+    fileReader.readAsArrayBuffer(blob);
+    if (type == "wrong_type") {
+        return false;
+    }
+    return true;
+};
+
+const _deleteCssClassesFileInput = (elem) => {
+    if (elem.classList.contains("error-annotation")) {
+        elem.classList.remove("error-annotation");
+    }
+
+    if (elem.classList.contains("error-visible")) {
+        elem.classList.remove("error-visible");
+    }
+
+    if (elem.classList.contains("file-downloaded")) {
+        elem.classList.remove("file-downloaded");
+    }
+};
+
+const _drawPasswordError = (errorPasswordElement) => {
+    _replaceTwoCssClasses(errorPasswordElement, "error-annotation", "error-visible");
+    _replaceTwoCssClasses(document.getElementById("password"), "input-valid", "input-error");
+    _replaceTwoCssClasses(document.getElementById("password-confirmation"), "input-valid", "input-error");
+};
+
+export const textFieldsVaildation = (changedForms) => {
+    if (changedForms.size == 0) {
+        const errorMain = document.getElementById("error_top");
+        _deleteCssClassesFileInput(errorMain);
+        errorMain.classList.add("error-visible");
+
+        errorMain.innerHTML = "Нет изменений для сохранения.";
+        return { fieldsAreValid: false, changes: {} };
+    }
+
+    const changes = {};
+    const passwordWasEntered = changedForms.has("password");
+    const passwordConfirmationWasEntered = changedForms.has("password-confirmation");
+    let error = false;
+
+    const errorPasswordElement = document.getElementById("error_password");
+    if (passwordWasEntered && passwordConfirmationWasEntered) {
+        if (_passwordsAreEqual(changedForms.get("password"), changedForms.get("password-confirmation"))) {
+            if (_passwordIsValid(changedForms.get("password"))) {
+                changes["password"] = changedForms.get("password");
+                _replaceTwoCssClasses(errorPasswordElement, "error-visible", "error-annotation");
+                _replaceTwoCssClasses(document.getElementById("password"), "input-error", "input-valid");
+                _replaceTwoCssClasses(document.getElementById("password-confirmation"), "input-error", "input-valid");
+            } else {
+                _drawPasswordError(errorPasswordElement);
+                errorPasswordElement.innerHTML = "Пароль должен содержать от 5 до 12 символов, одну цифру и одну букву.";
+                error = true;
+            }
+        } else {
+            _drawPasswordError(errorPasswordElement);
+            errorPasswordElement.innerHTML = "Пароли не совпадают.";
+            error = true;
+        }
+    } else if (passwordWasEntered || passwordConfirmationWasEntered) {
+        _drawPasswordError(errorPasswordElement);
+        errorPasswordElement.innerHTML = "Для смены пароля заполните два поля.";
+        error = true;
+    }
+
+    const errorUsernameElement = document.getElementById("error_username");
+    if (changedForms.has("username")) {
+        const username = changedForms.get("username");
+        if (!_usernameIsValid(username)) {
+            _replaceTwoCssClasses(errorUsernameElement, "error-annotation", "error-visible");
+            _replaceTwoCssClasses(document.getElementById("username"), "input-valid", "input-error");
+            errorUsernameElement.innerHTML = "Логин должен содержать минимум 3 символа.";
+            error = true;
+        } else {
+            changes["username"] = changedForms.get("username");
+            _replaceTwoCssClasses(errorUsernameElement, "error-visible", "error-annotation");
+            _replaceTwoCssClasses(document.getElementById("username"), "input-error", "input-valid");
+        }
+    }
+
+    const errorEmailElement = document.getElementById("error_email");
+    if (changedForms.has("email")) {
+        const email = changedForms.get("email");
+        if (!_emailIsValid(email)) {
+            _replaceTwoCssClasses(errorEmailElement, "error-annotation", "error-visible");
+            _replaceTwoCssClasses(document.getElementById("email"), "input-valid", "input-error");
+            errorEmailElement.innerHTML = "Недопустимый email.";
+            error = true;
+        } else {
+            changes["email"] = changedForms.get("email");
+            _replaceTwoCssClasses(errorEmailElement, "error-visible", "error-annotation");
+            _replaceTwoCssClasses(document.getElementById("email"), "input-error", "input-valid");
+        }
+    }
+
+    if (error) {
+        return { fieldsAreValid: false, changes: {} };
+    }
+    return { fieldsAreValid: true, changes: changes };
+
+};
