b56a18fa725b0317d94dd42f5ed6fa554c261ad9
EgorKekor <egor140398@gmail.com>
2019-10-25 11:26:31 +0300

    Service warker fixed and tested.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -11,18 +11,27 @@ import SingleGameV from "./view/singleGameV.js";
 
 
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", function() {
-//       navigator.serviceWorker.register("./sw.js").then(function(registration) {
-//           console.log("ServiceWorker registration successful with scope: ", registration.scope);
-//       }).catch(function(err) {
-//           console.log("ServiceWorker registration failed: ", err);
-//       });
-//   });
-// }
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js").then(function (registration) {
+        console.log("ServiceWorker registration successful with scope: ", registration.scope);
+    }).catch(function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+    });
+}
+
+// navigator.serviceWorker.addEventListener("message", function handler(event) {
+//     console.log("first data");
+//     console.log(event.data.synSobaki);
+// });
+
+// navigator.serviceWorker.addEventListener("message", function handler(event) {
+//     console.log("second data");
+//     console.log(event.data.synSobaki);
+// });
 
 // navigator.serviceWorker.getRegistrations().then(function (registrations) {
 //   for (let registration of registrations) {
+//       console.log("UNREGISTER");
 //     registration.unregister();
 //   }
 // });
@@ -38,10 +47,3 @@ router.register(PROFILE, ProfileV);
 router.register(SINGLE_GAME, SingleGameV);
 
 router.start();
-
-
-
-// import Template from "../css/WEB_LAB.pug"
-
-// const root = document.getElementById("application");
-// root.insertAdjacentHTML("beforeend", Template());
--- frontend/src/js/sw.js
+++ frontend/src/js/sw.js
@@ -1,25 +1,52 @@
-const CACHE_NAME = "svoyak-v1";
+const CACHE_NAME = "svoyak-v1.0.2";
 const urlsToCache = [
     "/",
-    "/bundle.js",
-    "/user/profile",
-
+    "/bundle.js"
 ];
 
+const applicationPages = [
+    "/login",
+    "/signup",
+];
+
+
 async function cacheInitPromise() {
-    const cache = await caches.open(CACHE_NAME);
-    console.log("Opened cache");
-    await cache.addAll(urlsToCache);
+    try {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(urlsToCache);
+    } catch (err) {
+        console.log(`Cache init error ${err}`);
+    }
     console.log("Установлен успешно");
 }
 
+self.addEventListener("install", function (event) {
+    event.waitUntil(cacheInitPromise());
+});
+
+//  ===========================================================
+
+// TODO: Добавить кэширование переменных данных, которые по сообщению
+// message из вне будут изменяться в кэше
+
 async function processPromise(event) {
-    console.log("HERE!");
-    let response = await caches.match(event.request);
+    const url = new URL(event.request.url);
+    if (applicationPages.includes(url.pathname) === true) {
+        const cache = await caches.open(CACHE_NAME);
+        const response = await cache.match("/");
+        console.log(`Страница приложения найдена в кэше: ${response.url}`);
+        return response;
+    }
+
+
+
+    const response = await caches.match(event.request);
     if (response) {
         console.log(`Найдено в кэше: ${response.url}`);
         return response;
     }
+
+
 
     let fetchRequest = event.request.clone();
 
@@ -29,22 +56,35 @@ async function processPromise(event) {
         return response;
     }
 
-    let responseToCache = response.clone();
-    let cache = await caches.open(CACHE_NAME);
-    cache.put(event.request, responseToCache);
-    console.log(`Обычный запрос: ${response.url}`);
+    // let responseToCache = response.clone();
+    // let cache = await caches.open(CACHE_NAME);
+    // cache.put(event.request, responseToCache);
+    // console.log(`Обычный запрос: ${response.url}`);
     return response;
 }
-
-
-self.addEventListener("install", function (event) {
-    event.waitUntil(cacheInitPromise());
-});
-
-self.addEventListener("activate", (event) => {
-    console.log("Активирован");
-});
 
 self.addEventListener("fetch", function (event) {
     event.respondWith(processPromise(event));
 });
+
+// ============================================================
+
+async function reloadPromise() {
+    const cacheNames = await caches.keys();
+
+    const promises = cacheNames.map(async (cacheName) => {
+        if (cacheName !== CACHE_NAME) {
+            const result = await caches.delete(cacheName);
+            return result
+        }
+    });
+
+    return await Promise.all(promises);
+}
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(reloadPromise());
+});
+
+// ============================================================
+
