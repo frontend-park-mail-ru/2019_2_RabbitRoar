711d5aa6b05f7d49f0587d740f381c0672864336
Anita <smirnovasiberia@gmail.com>
2019-11-08 16:49:47 +0300

    Unautorised users can play offline game.

--- frontend/src/js/controller/tabsC.js
+++ frontend/src/js/controller/tabsC.js
@@ -23,7 +23,7 @@ class TabsC {
         this.registerClassHandler(".tab-click", "mouseout", this._unLightTab.bind(this));
 
         this.registerClassHandler(".join-button", "click", this._joinClick.bind(this));
-        this.registerClassHandler(".join-button-training", "click", this._joinTraining.bind(this));
+       // this.registerClassHandler(".join-button-training", "click", this._joinTraining.bind(this));
 
         this.registerClassHandler(".popup-button", "click", this._processPopUp.bind(this));
         this.registerClassHandler(".create-room-btn", "click", this._routeToRoomCreation.bind(this));
@@ -70,16 +70,11 @@ class TabsC {
             return;
         }
     }
-
-    _joinTraining(){
-        alert("join offline");
-    }
     
     _joinClick() {
         const continueBtn = document.getElementById("continue");
-
         if (continueBtn) {
-            if (!ValidatorF.checkLocalstorageAutorization()) {
+            if (!ValidatorF.checkLocalstorageAutorization() && !document.getElementById("offline_mode")) {
                 continueBtn.id = "login";
                 document.getElementById("popup-elem-top").innerHTML = "Для игры необходимо авторизоваться";
             } else {
