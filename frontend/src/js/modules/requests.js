28562f8be57889761ec51cd17e9aa732b1fedde0
EgorKekor <egor140398@gmail.com>
2019-11-13 18:36:52 +0300

    SW cache update system.

--- frontend/src/js/modules/requests.js
+++ frontend/src/js/modules/requests.js
@@ -3,7 +3,26 @@ import { SERVICE_WORKER_CMD } from "../modules/events.js";
 import Bus from "../event_bus.js";
 
 
+const userCacheDelete = () => {
+    const base = "https://svoyak.fun/api";
+
+    if (window.navigator.onLine) {
+        Bus.emit(SERVICE_WORKER_CMD, {
+            command: "delete",
+            url: base + "/user/"
+        });
+
+        Bus.emit(SERVICE_WORKER_CMD, {
+            command: "regExp_delete",
+            regExp: new RegExp("^(/api/uploads/avatar/)[0-9]+(.jpeg|.png)$"),
+        });
+    }
+}
+
+
 export async function signIn(login, password) {
+    userCacheDelete();
+
     const body = JSON.stringify({
         "username": login,
         "password": password
@@ -18,15 +37,19 @@ export async function signIn(login, password) {
 }
 
 export async function logout() {
+    userCacheDelete();
+
     let response = await deleteRequest("/logout");
 
     if (response.status === 401) {
         localStorage.removeItem("authorized");
         //throw new Error(`Logout error: ${response.statusText}`);
-    } 
+    }
 }
 
 export async function signUp(userStructure) {
+    userCacheDelete();
+
     let response = await postRequest("/signup", JSON.stringify(userStructure));
 
     if (!response.ok) {
@@ -35,6 +58,8 @@ export async function signUp(userStructure) {
 }
 
 export async function changeAvatar(formData, csrf) {
+    userCacheDelete();
+
     const headers = {
         "X-Csrf-Token": csrf,
     }
@@ -45,10 +70,7 @@ export async function changeAvatar(formData, csrf) {
 }
 
 export async function changeTextFields(changesMap, csrf) {
-    Bus.emit(SERVICE_WORKER_CMD, {
-        command: "delete",
-        url: "/user/"
-    });
+    userCacheDelete();
 
     const headers = {
         "Content-type": "application/json",
@@ -96,7 +118,7 @@ export async function getUserInfo() {
 
 export async function getCSRF() {
     let response = await getRequest("/csrf");
-    
+
     if (!response.ok) {
         throw new Error(`Cannot get user info: ${response.statusText}`);
     }
@@ -104,14 +126,6 @@ export async function getCSRF() {
     return obj;
 }
 
-export async function getWS() {
-    let response = await getRequest("/game/ws");
-    if (!response.ok) {
-        throw new Error(`Cannot install websocket connection: ${response.statusText}`);
-    }
-    const obj = await response.json();
-    return obj;
-}
 
 export async function postCreateRoom(roomOptions) {
     let response = await postRequest("/game", JSON.stringify(roomOptions));
--- frontend/src/js/workers/sw.js
+++ frontend/src/js/workers/sw.js
@@ -1,5 +1,8 @@
 const CACHE_NAME = "svoyak-v1.0.5";
 
+// Когда мы передаем URL строку в функции cache API,
+// неявно создается объект реквеста, где урл запроса сгенериться,
+// автоматически из имени домена в строке
 
 async function cacheInitPromise() {
     self.preCacheList = new Array;
@@ -51,10 +54,10 @@ async function processPromise(event) {
         const cache = await caches.open(CACHE_NAME);
         try {
             const response = await cache.match(_getUrlRevision(requestUrl));
-            console.log(`OFFLINE Страница найдена в кэше!!!: ${response.url}`);
+            console.log(`OFFLINE Страница найдена в кэше: ${response.url}`);
             return response;
         } catch {
-            console.log(`Страница не найдена!`);
+            console.log(`Страница не найдена: ${requestUrl}`);
             return;
         }
     } else {
@@ -68,28 +71,29 @@ async function processPromise(event) {
             console.log(`Был удален: ${_getUrlRevision(requestUrl)}`);
             await cache.add(_getUrlRevision(requestUrl));
             response = await cache.match(_getUrlRevision(requestUrl));
-            //TO_DO: Добавить защиту если не восстановил
-            console.log("Восстановлен успешно");
-            console.log(response);
+
+            if (response) {
+                console.log("Восстановлен успешно");
+                console.log(response);
+                return response;
+            } else {
+                console.log("Не удалось восстановить");
+            }
+        }
+
+
+        if (!needCache(requestUrl)) {
+            response = await fetch(event.request);
+            console.log(`Не нужно кэшировать: ${requestUrl.pathname}`);
             return response;
         }
 
-        let fetchRequest = event.request.clone();
+
+        const fetchRequest = event.request.clone();
         response = await fetch(fetchRequest);
-
-        if (response.headers.get("Content-Length") === null || response.headers.get("Content-Length") === 0) {
-            console.log("Response not have Content-Lenght");
-            console.log(response);
-            return response;
-        }
-        if (requestUrl.pathname === "/api/csrf") {
-            return response;
-        }
-
-
         const responseToCache = response.clone();
 
-        cache.put(event.request, responseToCache);
+        cache.put(requestUrl, responseToCache);
         console.log(`ONLINE Страница загружена и сохранена в кэше!: ${requestUrl}`);
         return response;
     }
@@ -113,6 +117,21 @@ async function reloadPromise() {
         }
     });
 
+
+    const cache = await caches.open(CACHE_NAME);
+
+    for (const cacheObj of self.__precacheManifest) {
+        const keys = await cache.keys();
+        for (request of keys) {
+            const requestUrl = new URL(request.url);
+            if ((requestUrl.pathname === cacheObj.url) && (requestUrl.search !== "?" + cacheObj.revision)) {
+                const delPromise = await cache.delete(requestUrl.pathname + requestUrl.search);
+                promises.push(delPromise);
+                console.log(`Удалена старая версия кэша: ${requestUrl.pathname + requestUrl.search}`);
+            }
+        }
+    }
+
     return await Promise.all(promises);
 }
 
@@ -127,8 +146,20 @@ self.addEventListener("message", async function (event) {
 
     if (event.data.command === "delete") {
         const cache = await caches.open(CACHE_NAME);
-        console.log(`DELETE ${event.data.url}`);    
+        console.log(`delete ${event.data.url}`);
         await cache.delete(event.data.url);
+    } else if (event.data.command === "regExp_delete") {
+        const cache = await caches.open(CACHE_NAME);
+        const keys = await cache.keys();
+
+        for (key of keys) {
+            const parseUrl = new URL(key.url);
+            console.log(parseUrl.pathname);
+            if (parseUrl.pathname.match(event.data.regExp)) {
+                const result = await cache.delete(key);
+                console.log(`regExp_delete[${key}]: ${result}`);
+            }
+        }
     }
 
     // const clients = await self.clients.matchAll();
@@ -140,6 +171,23 @@ self.addEventListener("message", async function (event) {
     //     client.postMessage("!!!!!");
     // });
 });
+
+
+const CacheListApi = [
+    "^(/api/user/)$",
+    "^(/api/profile)$",
+    "^(/api/uploads/avatar/)[0-9]+(.jpeg|.png)$",
+];
+
+function needCache(requestUrl) {
+    for (path of CacheListApi) {
+        const regExp = new RegExp(path);
+        if (requestUrl.pathname.match(regExp)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 function _isPreCacheUrl(requestUrl) {
@@ -168,8 +216,8 @@ function _getUrlRevision(requestUrl) {
                 return requestUrl.pathname + "?" + cacheObj.revision;
             }
         }
-        return requestUrl;
     }
+    return requestUrl;
 }
 
 
@@ -185,6 +233,7 @@ function _transformApiUrl(requestUrl) {
 
     return requestUrl;
 }
+
 
 
 
