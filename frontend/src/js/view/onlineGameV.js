83329a1ee245d3b74effba07f6c987a047ff4faf
Anita <smirnovasiberia@gmail.com>
2019-11-25 13:13:42 +0300

    UsersGamePanelE was added to view.

--- frontend/src/js/view/onlineGameV.js
+++ frontend/src/js/view/onlineGameV.js
@@ -2,7 +2,7 @@ import NavbarE from "../element/navbarE.js";
 import NetworkWarningE from "../element/networkWarningE.js";
 import QuestionTableE from "../element/questionTableE";
 import GamePanelE from "../element/gamePanelE";
-
+import UsersGamePanelE from "../element/usersGamePanelE";
 
 
 import { View } from "./view.js";
@@ -13,6 +13,7 @@ class OnlineGameV extends View{};
 export default new OnlineGameV(
     document.getElementById("application"),
     NavbarE,
+    UsersGamePanelE,
     QuestionTableE,
     GamePanelE,
     NetworkWarningE
