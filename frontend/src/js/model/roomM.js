9b9f18b8e6d4ba218a17e40d2397629e59ce6449
Anita <smirnovasiberia@gmail.com>
2019-11-27 02:27:59 +0300

    Fixed request for room creation.

--- frontend/src/js/model/roomM.js
+++ frontend/src/js/model/roomM.js
@@ -139,26 +139,26 @@ class RealRoomM {
             }
             this.roomId = response.UUID;
             console.log("ROOM ID from create:", this.roomId);
-        }
+        } else {
+            try {
+                const csrf = await getCSRF();
+                await deleteLeaveRoom(csrf.CSRF);
+            } catch (err) {
+                console.log(err);
+                this.lastState = this.state;
+                this.state = "crash_connection";
+                return;
+            }
 
-        try {
-            const csrf = await getCSRF();
-            await deleteLeaveRoom(csrf.CSRF);
-        } catch (err) {
-            console.log(err);
-            this.lastState = this.state;
-            this.state = "crash_connection";
-            return;
-        }
-
-        try {
-            const csrf = await getCSRF();
-            response = await postJoinRoom(this.roomId, csrf.CSRF);
-        } catch (err) {
-            console.log(err);
-            this.lastState = this.state;
-            this.state = "crash_connection";
-            return;
+            try {
+                const csrf = await getCSRF();
+                response = await postJoinRoom(this.roomId, csrf.CSRF);
+            } catch (err) {
+                console.log(err);
+                this.lastState = this.state;
+                this.state = "crash_connection";
+                return;
+            }
         }
 
         this.roomInfo = response;
